eaabba3bcc9eca1475ff3285871b3272adc92abb
Nathaniel Seymour <nathaniel.seymour@gmail.com>
2017-09-03 23:03:27 +0200

    Install packages 'js2-mode', 'expand-region' and 'web-mode'

--- init.el
+++ init.el
@@ -1,5 +1,16 @@
 ;; load utility functions
 (load-file "utils.el")
 
+;; Add package repository Melpa
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
+(package-initialize)
+
+;; List of external packages to install
+(setq nat-packages
+      '(js2-mode web-mode expand-region))
+
+(nat-install-packages nat-packages)
+
 (nat-load-config-file "prog-mode")
 (nat-load-config-file "display")

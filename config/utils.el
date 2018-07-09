3d4798128e1b9238d38185c3399a870c127d83cb
nathaniel.seymour@gmail.com <nathaniel.seymour.external@thalesgroup.com>
2018-07-09 15:20:20 +0200

    Add deleted file config/utils.el

---
+++ config/utils.el
@@ -0,0 +1,11 @@
+(defun nat-load-config-file (filename)
+  "Load a configuration file in the directory 'config'"
+  (load-file (expand-file-name (concat filename ".el") "config")))
+
+(defun nat-install-packages (packages)
+  "Install external packages of the list 'packages'"
+
+  ;; Install each package of the list if not installed
+  (dolist (package packages)
+    (when (not (package-installed-p package))
+    (package-install package))))
